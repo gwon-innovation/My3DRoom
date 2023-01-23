a6a40a7c61600d50969ada9ac14fc63da7a8d3be
gwon.innovation <gwon.innovation@gmail.com>
2023-01-24 02:09:56 +0900

    feat: Home에 emotiond으로 배경 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const containerStyle = css({
+    width: "100%",
+    height: "100vh",
+    background: "#121212",
+    color: "#FFFFFF"
+  });
+  return <div css={containerStyle}>Home</div>;
 }
