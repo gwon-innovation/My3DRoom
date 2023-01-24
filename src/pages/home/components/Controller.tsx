ea517b029076ab1501c1214cd392be6799cb0b84
Hyeokshin Gwon <76881466+gwon-innovation@users.noreply.github.com>
2023-01-24 21:36:48 +0900

    Feature/add floor controller (#3)
    * feat: Floor size controller 추가
    
    * feat: Home화면에 controller 추가

---
+++ src/pages/home/components/Controller.tsx
@@ -0,0 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { FloorController, IFloorControllerProps } from "./FloorController";
+
+interface ControllerProps {
+  floorProps: IFloorControllerProps;
+}
+
+export function Controller({ floorProps }: ControllerProps) {
+  const controllerStyle = css({
+    position: "absolute",
+    top: "1.2rem",
+    right: "1.2rem",
+    marginLeft: "auto",
+    padding: "1.2rem",
+    boxSizing: "border-box",
+    width: "20rem",
+    height: "50%",
+    background: "#343434",
+    borderRadius: "1rem",
+    overflow: "auto",
+    zIndex: 100
+  });
+
+  return (
+    <div css={controllerStyle}>
+      <p>Floor</p>
+      <FloorController
+        floorXZ={floorProps.floorXZ}
+        setFloorXZ={floorProps.setFloorXZ}
+      />
+    </div>
+  );
+}
