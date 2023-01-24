c63a262428f7d5fa10d658183529091792e56484
gwon.innovation <gwon.innovation@gmail.com>
2023-01-24 19:47:31 +0900

    feat: 컴포넌트들 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,36 @@
+/* eslint-disable react/no-unknown-property */
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { IFloorArea } from "models";
+import { Floor, WallFront, WallLeft } from "./planes";
+import { TopLight } from "./lights/TopLight";
 
 export default function Home() {
+  const [floorXZ, setFloorXZ] = useState<IFloorArea>({
+    x: 3,
+    z: 2.6
+  });
+
   const containerStyle = css({
     width: "100%",
     height: "100vh",
     background: "#121212",
     color: "#FFFFFF"
   });
-  return <div css={containerStyle}>Home</div>;
+
+  return (
+    <div css={containerStyle}>
+      <Canvas camera={{ position: [2, 4, 3.5] }}>
+        <OrbitControls />
+        <TopLight />
+
+        <WallLeft floorXZ={floorXZ} />
+        <WallFront floorXZ={floorXZ} />
+        <Floor floorXZ={floorXZ} />
+      </Canvas>
+    </div>
+  );
 }
